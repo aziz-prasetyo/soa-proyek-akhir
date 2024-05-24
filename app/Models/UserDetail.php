de39cbf14af3eba2799bfc8da60af0a29bd17fed
Muhammad Aziz Prasetyo <83118151+aziz-prasetyo@users.noreply.github.com>
2024-05-24 23:56:49 +0700

    Aziz/login (#5)
    * Model untuk User, UserDetail, dan Role
    
    * Mengubah UserDetails menjadi UserDetail

---
+++ app/Models/UserDetail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class UserDetail extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'user_details';
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The data type of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'gender',
+        'phone_number',
+    ];
+}
