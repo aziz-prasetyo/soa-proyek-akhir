292d997e7b43339da1c7dd37096c8c4d05eee725
aryabf <arya.bagus864@gmail.com>
2024-06-16 23:32:57 +0700

    Register

---
+++ app/Http/Controllers/Api/V1/AuthApiController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthApiController extends Controller
+{
+    /**
+     * User registration
+     */
+    public function register(RegisterRequest $request): JsonResponse
+    {
+        $userData = $request->validated();
+
+        $userData['email_verified_at'] = now();
+        $user = User::create($userData);
+
+        $response = Http::post(env('APP_URL') . '/oauth/token', [
+            'grant_type' => 'password',
+            'client_id' => env('PASSPORT_PASSWORD_CLIENT_ID'),
+            'client_secret' => env('PASSPORT_PASSWORD_SECRET'),
+            'username' => $userData['email'],
+            'password' => $userData['password'],
+            'scope' => '',
+        ]);
+
+        $user['token'] = $response->json();
+
+        return response()->json([
+            'success' => true,
+            'statusCode' => 201,
+            'message' => 'User has been registered successfully.',
+            'data' => $user,
+        ], 201);
+    }
+
+}
